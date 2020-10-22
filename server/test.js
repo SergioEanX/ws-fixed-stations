72d3480f72a8199613f3f5f29a544bef5dc17efb
eanx <sergio.ferlito@enea.it>
2020-10-22 14:21:37 +0200

    quick test code

---
+++ server/test.js
@@ -0,0 +1,6 @@
+// const path= require('path')
+// require('dotenv').config({path:path.join(__dirname,'.env')})
+// console.log(process.env.MONGO_DB,
+//   process.env.COLLECTION_TO_MONITOR,)
+// const {getChange} = require('./controllers/contrChangeStream');
+// getChange()
