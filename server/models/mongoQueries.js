b2f7dee8d40c6cc66a0c86877a72342b9c835127
eanx <sergio.ferlito@enea.it>
2020-10-26 18:21:46 +0100

    changed pipeline

--- server/models/mongoQueries.js
+++ server/models/mongoQueries.js
@@ -1,27 +1,57 @@
-exports.getAQIdatag = async (coll, ) => {
-    const pipeline = [
-      {
-        $match: {
-          _id: id,
+const {
+  calculateAQI,
+  getAQIInfo,
+} = require('../controllers/contrAQI');
+exports.getAQIdata = async (coll) => {
+  const pipeline = [
+    {
+      $project: {
+        date: {
+          $dateToString: {
+            date: '$_id',
+            format: '%Y-%m-%d',
+          },
+        },
+        station: 1,
+        PM10: '$data.pm10_mg_m3',
+      },
+    },
+    {
+      $match: {
+        date: '2020-10-20',
+      },
+    },
+    {
+      $group: {
+        _id: '$station',
+        PM10arr: {
+          $push: '$PM10',
+        },
+        PM10avg: {
+          $avg: '$PM10',
         },
       },
-      ...aggregationAvg,
-    ];
-  
-    // Aggregation framework to compute AVG
-    const cursor = coll.aggregate(pipeline);
-    for (
-      let docAQI = await cursor.next();
-      docAQI;
-      docAQI = await cursor.next()
-    ) {
-      console.log(JSON.stringify(doc_avg), null, 2);
-      resolve(doc)
-  
-      // await coll_avg.updateOne(
-      //   { _id: doc_avg._id },
-      //   { $set: doc_avg },
-      //   { upsert: true },
-      // );
-    }
-  };
+    },
+  ];
+
+  // Aggregation framework to compute AVG
+  const cursor = coll.aggregate(pipeline);
+  let result = [];
+  for (
+    let docAQI = await cursor.next();
+    docAQI;
+    docAQI = await cursor.next()
+  ) {
+    console.log(JSON.stringify(docAQI), null, 2);
+    docAQI.AQI = calculateAQI(docAQI.PM10avgH24);
+    docAQI.AQIinfo = getAQIInfo(docAQI.AQI);
+    result.push(docAQI);
+
+    // await coll_avg.updateOne(
+    //   { _id: doc_avg._id },
+    //   { $set: doc_avg },
+    //   { upsert: true },
+    // );
+  }
+  return result;
+};
