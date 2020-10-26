3ea925709bd4eb9701eb50d93f6c46b02b731045
eanx <sergio.ferlito@enea.it>
2020-10-26 18:22:41 +0100

    modified code for AQI and  AQII info

--- server/controllers/contrAQI.js
+++ server/controllers/contrAQI.js
@@ -8,22 +8,22 @@ const {
   POLLUTANT_UNITS,
 } = require('../utils/aqi');
 
-function binarySearch(array, el) {
-  var m = 0;
-  var n = array.length - 1;
-  while (m <= n) {
-    var k = (n + m) >> 1;
-    var cmp = el - array[k];
-    if (cmp > 0) {
-      m = k + 1;
-    } else if (cmp < 0) {
-      n = k - 1;
-    } else {
-      return [k, k + 1];
-    }
-  }
-  return [n, n + 1];
-}
+// function binarySearch(array, el) {
+//   var m = 0;
+//   var n = array.length - 1;
+//   while (m <= n) {
+//     var k = (n + m) >> 1;
+//     var cmp = el - array[k];
+//     if (cmp > 0) {
+//       m = k + 1;
+//     } else if (cmp < 0) {
+//       n = k - 1;
+//     } else {
+//       return [k, k + 1];
+//     }
+//   }
+//   return [n, n + 1];
+// }
 
 const test = [
   {
@@ -50,12 +50,14 @@ const test = [
   },
 ];
 
-const calculateAQI = (
+// if a pollutant is not specified in inputs it is undefined
+exports.calculateAQI = (
   PM10Mean_h24,
   O3MaxRunningMean,
   NO2Maxh,
   NO2Meanh24,
 ) => {
+  // subindex return NaN when input in undefined
   const SPM10 = (PM10Mean_h24 / LIMIT_VALUES.VL_PM10) * 1000;
   const SO3 = (O3MaxRunningMean / LIMIT_VALUES.VL_O3) * 100;
   const SNO2 =
@@ -63,18 +65,19 @@ const calculateAQI = (
     (NO2Maxh / LIMIT_VALUES.VL_NO2o +
       NO2Meanh24 / LIMIT_VALUES.VL_NO2a) *
     100;
-  const AQInumber = Math.max(...[SPM10, SO3, SNO2]);
+    // filter out NaN values in array
+  const AQInumber = Math.max(...[SPM10, SO3, SNO2].filter(v=>!isNaN(v)));
   return AQInumber;
 };
-calculateAQI(10, 30, 40, 20);
 
-const getAQIInfo = (AQIn) => {
+
+exports.getAQIInfo = (AQIn) => {
   const AQIinfoObj = POLLUTANT_LABEL_COLOR_REF.filter((el) => {
     return AQIn >= el.AQI[0] && AQIn < el.AQI[1];
   });
   return AQIinfoObj[0];
 };
 
-console.log(JSON.stringify(getAQIInfo(20)));
+//console.log(JSON.stringify(getAQIInfo(20)));
 
 //https://aqicn.org/city/italy/campania/portici/portici-parco-reggia/
