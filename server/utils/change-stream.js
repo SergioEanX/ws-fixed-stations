59dd17f32c1fef5ca38687f93428e3729cbca8c5
eanx <sergio.ferlito@enea.it>
2020-10-27 15:43:32 +0100

    formatting fix

--- server/utils/change-stream.js
+++ server/utils/change-stream.js
@@ -3,25 +3,25 @@ const { getCollection } = require('./mongo-client');
 
 // return change stream Object
 // MongoDB database and collection to monitor are set in .env
-exports.getChangeStream = async function (db,coll) {
-  //const resumeToken = await getResumetoken("SOME_UPSERT_TOKEN_ID");
+exports.getChangeStream = async function (db, coll) {
+  // const resumeToken = await getResumetoken("SOME_UPSERT_TOKEN_ID");
   const changeStream = (await getCollection(db, coll)).watch(
     [
       {
         $match: {
           operationType: {
-            $in: ['insert', 'update', 'replace'], //do not watch deletes
+            $in: ['insert', 'update', 'replace'], // do not watch deletes
           },
         },
       },
       {
         $project: {
-          documentKey: false, //documentKey is useless
+          documentKey: false, // documentKey is useless
         },
       },
     ],
     { fullDocument: 'updateLookup' },
-  ); //"resumeAfter": resumeToken,
+  ); // "resumeAfter": resumeToken,
 
   return changeStream;
 };
