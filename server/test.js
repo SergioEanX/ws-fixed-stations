b5806f61e256864686c5d76a62ed8d6e66c84292
eanx <sergio.ferlito@enea.it>
2020-10-27 14:58:01 +0100

    test AQI

--- server/test.js
+++ server/test.js
@@ -4,3 +4,13 @@
 //   process.env.COLLECTION_TO_MONITOR,)
 // const {getChange} = require('./controllers/contrChangeStream');
 // getChange()
+const { POLLUTANT_LABEL_COLOR_REF } = require('./utils/aqi');
+
+const getAQIInfo = (AQIn) => {
+  const AQIinfoObj = POLLUTANT_LABEL_COLOR_REF.filter((el) => {
+    return AQIn >= el.AQI[0] && AQIn < el.AQI[1];
+  });
+  return AQIinfoObj[0];
+};
+
+console.log(JSON.stringify(getAQIInfo(7.96), null, 2));
