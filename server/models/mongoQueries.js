9480aa9eb981a996292ed02133ff46cae2f698e4
eanx <sergio.ferlito@enea.it>
2020-10-27 09:16:32 +0100

    modified pipeline to accept datetime

--- server/models/mongoQueries.js
+++ server/models/mongoQueries.js
@@ -2,24 +2,49 @@ const {
   calculateAQI,
   getAQIInfo,
 } = require('../controllers/contrAQI');
-exports.getAQIdata = async (coll) => {
+
+const getDateTime = (strDateTime) => {
+  return strDateTime ? new Date(strDateTime) : new Date();
+};
+
+exports.getAQIdata = async (coll, strDateTime) => {
+  // set current datetime according to input string datetime
+  // if not specified is the actual datetime
+  const currentDateTime = getDateTime(strDateTime);
+
+  // compute one day before
+  const oneDayBefore = new Date(
+    getDateTime(strDateTime).setDate(
+      getDateTime(strDateTime).getDate() - 1,
+    ),
+  );
+
+  // set datetime filter  pipeline stage
+  const stageDatesFilter = {
+    _id: { $gte: oneDayBefore, $lt: currentDateTime },
+  };
+
   const pipeline = [
+    // {
+    //   $project: {
+    //     date: {
+    //       $dateToString: {
+    //         date: '$_id',
+    //         format: '%Y-%m-%d',
+    //       },
+    //     },
+    //     station: 1,
+    //     PM10: '$data.pm10_mg_m3',
+    //   },
+    // },
     {
-      $project: {
-        date: {
-          $dateToString: {
-            date: '$_id',
-            format: '%Y-%m-%d',
-          },
-        },
-        station: 1,
-        PM10: '$data.pm10_mg_m3',
-      },
+      $match: stageDatesFilter,
+      // {
+      //   date: '2020-10-20',
+      // },
     },
     {
-      $match: {
-        date: '2020-10-20',
-      },
+      $project: { station: 1, PM10: '$data.pm10_mg_m3' }, // ADD MORE POLLUTANT HERE
     },
     {
       $group: {
@@ -42,9 +67,12 @@ exports.getAQIdata = async (coll) => {
     docAQI;
     docAQI = await cursor.next()
   ) {
-    console.log(JSON.stringify(docAQI), null, 2);
+    // console.log(JSON.stringify(docAQI), null, 2);
+    // compute AQI according to ARPAC spec
     docAQI.AQI = calculateAQI(docAQI.PM10avgH24);
+    // retrive AQI info (color code, description,etc..)
     docAQI.AQIinfo = getAQIInfo(docAQI.AQI);
+    // put all AQI by station into an array
     result.push(docAQI);
 
     // await coll_avg.updateOne(
