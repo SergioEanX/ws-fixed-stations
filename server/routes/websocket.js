0b0f20b4810bcf7a771b08f7982f06373483afea
eanx <sergio.ferlito@enea.it>
2020-10-28 19:44:20 +0100

    added /health route

--- server/routes/websocket.js
+++ server/routes/websocket.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const pjson = require('../package.json');
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get("/health", function (req, res, next) {
+  res.status(200).json({status:'success',data:`App ${pjson.name} is healthy on date ${new Date()}`})
+  // to serve VUE file
+  // res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
